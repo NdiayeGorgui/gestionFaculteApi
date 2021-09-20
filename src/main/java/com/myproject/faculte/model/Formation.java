ac14a35e576312d506b89deae762a09ac6f40743
Gorgui Ndongo <gorguindongondiaye@gmail.com>
2021-09-20 12:01:45 -0400

    ajout liaison entre cours et formation

--- src/main/java/com/myproject/faculte/model/Formation.java
+++ src/main/java/com/myproject/faculte/model/Formation.java
@@ -1,13 +1,19 @@
 package com.myproject.faculte.model;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.List;
-
+import java.util.Set;
+import javax.persistence.JoinColumn;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -36,4 +42,15 @@ public class Formation {
 		@OneToMany(mappedBy = "formation")
 		@JsonIgnore
 		private List<Groupe> groupes;
+		
+		 @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+		    @JoinTable(name = "cours_formations",
+		            joinColumns = {
+		                    @JoinColumn(name = "formation_id", referencedColumnName = "id",
+		                            nullable = false, updatable = false)},
+		            inverseJoinColumns = {
+		                    @JoinColumn(name = "cours_id", referencedColumnName = "id",
+		                            nullable = false, updatable = false)})
+		 @JsonIgnore
+		 private Set<Cour> cours = new HashSet<>();
 }
