b0fcceb79492fb56f63cd5feca6a2c84f2123145
Gorgui Ndongo <gorguindongondiaye@gmail.com>
2021-09-20 12:01:27 -0400

    ajout liaison entre cours et formation

--- src/main/java/com/myproject/faculte/model/Cour.java
+++ src/main/java/com/myproject/faculte/model/Cour.java
@@ -1,15 +1,20 @@
 package com.myproject.faculte.model;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -32,5 +37,9 @@ public class Cour {
 		private Enseignant enseignant;
 	 @ManyToOne
 		private TypeCour typecour;
+	 
+	 @JsonIgnore
+	 @ManyToMany(mappedBy = "cours", fetch = FetchType.LAZY)
+	 private Set<Formation> formations= new HashSet<>();
 
 }
