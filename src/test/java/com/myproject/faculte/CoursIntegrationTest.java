7cad485ee9c12469a10a0b81d35e5e6a96ea748d
Gorgui Ndongo <gorguindongondiaye@gmail.com>
2022-02-27 22:13:51 -0500

    Ajout test ingration pour cours

---
+++ src/test/java/com/myproject/faculte/CoursIntegrationTest.java
@@ -0,0 +1,86 @@
+package com.myproject.faculte;
+
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+
+import com.myproject.faculte.model.Cour;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CoursIntegrationTest {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
+	@Test
+	// @Sql("/test.sql")
+	public void getAllCoursTest() {
+		List<Cour> cours = testRestTemplate.getForObject("http://localhost:" + port + "/api/Cours", List.class);
+
+		Assertions.assertNotNull(cours);
+		Assertions.assertEquals(7, cours.size());
+
+	}
+
+	@Test
+	public void getCoursByIdTest() {
+		Cour cours = testRestTemplate.getForObject("http://localhost:" + port + "/api/Cours/58", Cour.class);
+
+		Assertions.assertNotNull(cours.getId());
+		Assertions.assertEquals(58, cours.getId());
+		Assertions.assertEquals("Bases de données Oracle", cours.getLibelle());
+		Assertions.assertEquals(55, cours.getNbeHeure());
+
+	}
+
+	@Test
+	public void saveCoursTest() {
+		Cour cours = new Cour();
+
+		cours.setLibelle("english");
+		cours.setNbeHeure(77);
+
+		Cour response = testRestTemplate.postForObject("http://localhost:" + port + "/api/Cours/", cours, Cour.class);
+
+		Assertions.assertNotNull(response.getId());
+		Assertions.assertEquals("english", response.getLibelle());
+		Assertions.assertEquals(77, response.getNbeHeure());
+
+	}
+
+	@Test
+	public void updateCoursByIdTest() {
+		Cour cours = new Cour();
+		cours.setId(58L);
+		cours.setLibelle("Bases de données Oracle");
+		cours.setNbeHeure(55);
+
+		testRestTemplate.put("http://localhost:" + port + "/api/Cours/58", cours, Cour.class);
+
+		Assertions.assertNotNull(cours.getId());
+		Assertions.assertEquals(58, cours.getId());
+		Assertions.assertEquals("Bases de données Oracle", cours.getLibelle());
+		Assertions.assertEquals(55, cours.getNbeHeure());
+
+	}
+
+	@Test
+	public void deleteCoursByIdTest() throws Exception {
+		Cour cours = new Cour();
+		cours.setId(76L);
+
+		testRestTemplate.delete("http://localhost:" + port + "/api/Cours/76", cours, Cour.class);
+
+		Assertions.assertEquals(76, cours.getId());
+
+	}
+}
